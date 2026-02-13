ff57a2c69d5a5472d206c0ae7d036e5fec152ec1
1234abhibisht <abhibisht129@gmail.com>
2026-02-13 19:57:09 +0530

    Update 17_PrintAllElementsOfMatrixInSpiralOrder.c

--- 04_Arrays2DQuestions/17_PrintAllElementsOfMatrixInSpiralOrder.c
+++ 04_Arrays2DQuestions/17_PrintAllElementsOfMatrixInSpiralOrder.c
@@ -1,3 +1,6 @@
+// Problem: You are given a rectangular matrix of integers. Starting from the outer boundary, 
+// traverse the matrix in a clockwise manner and continue moving inward layer by layer until all elements are visited.
+
 #include <stdio.h>
 void sprialMatrix(int n, int m, int arr[n][m])
 {
@@ -9,25 +12,25 @@ void sprialMatrix(int n, int m, int arr[n][m])
     int count = 0;
     while (count < totalElements)
     {
-        for (int j = minrow; j <= maxcolumn; j++)
+        for (int j = minrow; j <= maxcolumn && count < totalElements; j++)
         {
             printf("%d ", arr[minrow][j]);
             count++;
         }
         minrow++;
-        for (int i = minrow; i <= maxrow; i++)
+        for (int i = minrow; i <= maxrow && count < totalElements; i++)
         {
             printf("%d ", arr[i][maxcolumn]);
             count++;
         }
         maxcolumn--;
-        for (int j = maxcolumn; j >= mincolumn; j--)
+        for (int j = maxcolumn; j >= mincolumn && count < totalElements; j--)
         {
             printf("%d ", arr[maxrow][j]);
             count++;
         }
         maxrow--;
-        for (int i = maxrow; i >= minrow; i--)
+        for (int i = maxrow; i >= minrow && count < totalElements; i--)
         {
             printf("%d ", arr[i][mincolumn]);
             count++;
